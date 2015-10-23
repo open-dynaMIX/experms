1191bb3c5c5637ccd1518989f186db28f8fd5b32
Fabio <fabio@dynamix-tontechnik.ch>
2015-10-23 13:52:30 +0200

    license removed

--- src/configfile/check_ownerandgroup.py
+++ src/configfile/check_ownerandgroup.py
@@ -1,24 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-Copyright (C) 2015, Fabio Rämi
-All rights reserved.
-
-GNU GPL v3.0 or later.
-http://www.gnu.org/licenses/gpl-3.0.txt
-Experms comes with absolutely no warranty!
-
-Experms is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-Experms is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with Experms.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 import sys
 import pwd
