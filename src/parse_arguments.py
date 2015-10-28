4ec59a2ada8b91876e57849c0ab746018c04bc32
Fabio <fabio@dynamix-tontechnik.ch>
2015-10-28 13:58:39 +0100

    help message order

--- src/parse_arguments.py
+++ src/parse_arguments.py
@@ -9,21 +9,21 @@ def parse_arguments():
                                formatter_class=argparse.
                                RawDescriptionHelpFormatter)
 
-      parser.add_argument("-d", "--debug", dest="debug",
-                                action="store_true", help="Print debug-"
-                                "messages.")
-
       parser.add_argument("-r", "--restore", dest="restore",
                                 action="store_true", help="Just restore all "
                                 "the permissions and exit.")
+
+      parser.add_argument("-c", "--count", dest="count",
+                                action="store_true", help="Count the "
+                                "directories to watch and exit.")
 
       parser.add_argument("-v", "--version", dest="version",
                                 action="store_true", help="Print the version "
                                 "and exit.")
 
-      parser.add_argument("-c", "--count", dest="count",
-                                action="store_true", help="Count the "
-                                "directories to watch and exit.")
+      parser.add_argument("-d", "--debug", dest="debug",
+                                action="store_true", help="Print debug-"
+                                "messages.")
 
       args = parser.parse_args()
 
