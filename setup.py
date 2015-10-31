adbdaa473105dabe48b8fc87327d270679f6bff4
Fabio <fabio@dynamix-tontechnik.ch>
2015-10-31 13:15:30 +0100

    move files to src/experms/

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(name='experms',
+      version='0.1.0',
+      description='File permission handler with inotify',
+      author='Fabio Rämi',
+      author_email='fabio[a]dynamix-tontechnik[dot]ch',
+      url='https://github.com/open-dynaMIX/experms',
+      packages=['experms'],
+     )
