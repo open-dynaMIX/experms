4f17d977c3631fcb56a83744ab672e7141383367
Fabio <fabio@dynamix-tontechnik.ch>
2015-11-07 07:44:39 +0100

    chmodf/chmodd can also be empty

--- src/experms/configfile/check_chmod.py
+++ src/experms/configfile/check_chmod.py
@@ -8,21 +8,21 @@ import re
 def check_chmod(parser, section, what, debug):
     if parser.has_option(section, what):
         tempchmod = parser.get(section, what)
-        chmod = verify_chmod(tempchmod)
-        if chmod:
-            if debug:
-                print >> sys.stderr, ("[debug] '%s' in section '%s' "
-                                      "is valid" % (what, section))
-        else:
-            chmod = False
-            print >> sys.stderr, ("Error in section '%s': '%s' is not a valid "
-                                  "setting." % (section, what))
-    else:
-        chmod = None
-        if debug:
-            print >> sys.stderr, ("[debug] '%s' in section '%s' not set."
-                                  % (what, section))
+        if not tempchmod == '':
+            chmod = verify_chmod(tempchmod)
+            if chmod:
+                if debug:
+                    print >> sys.stderr, ("[debug] '%s' in section '%s' "
+                                          "is valid" % (what, section))
+            else:
+                print >> sys.stderr, ("Error in section '%s': '%s' is not a valid "
+                                      "setting." % (section, what))
+            return chmod
 
+    chmod = None
+    if debug:
+        print >> sys.stderr, ("[debug] '%s' in section '%s' not set."
+                              % (what, section))
     return chmod
 
 
